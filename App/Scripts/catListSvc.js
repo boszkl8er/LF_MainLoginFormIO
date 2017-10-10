05b1e4fa786bb00126a5f51c5466b748b0836fdf
boszkl8er <kittipong.iti17@gmail.com>
2017-10-10 17:05:22 +0700

    Update code

--- App/Scripts/catListSvc.js
+++ App/Scripts/catListSvc.js
@@ -1,12 +1,9 @@
 ﻿'use strict';
 angular.module('catApp')
 .factory('catListSvc', ['$http', function ($http) { 
-	//var apiUrl = 'https://lfdemoapi.portal.azure-api.net';
-	//var apiUrl = 'https://sukanya-tst-api.portal.azure-api.net';
-	
-	//From tun
-	//var apiUrl = 'https://tgtapidemo.azure-api.net';
+
 	var apiUrl = 'https://together.azure-api.net';
+	
     return {
         getItems : function(){
             return $http.get(apiUrl + '/api/CatList');
