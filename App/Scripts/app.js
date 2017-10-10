812ea07bd96e5e9bce143b77891eb7602b6c2e96
boszkl8er <kittipong.iti17@gmail.com>
2017-10-10 17:08:10 +0700

    Update code

--- App/Scripts/app.js
+++ App/Scripts/app.js
@@ -22,13 +22,7 @@ angular.module('catApp', ['ngRoute','AdalAngular'])
         {
             instance: 'https://login.microsoftonline.com/', 
             tenant: 'together.at',
-			//tenant: '3e0b6cbd-4959-4d01-81bf-ce883ddabd96',
-			//clientId: '0a07a77d-ee2e-477e-93ca-fd7f59421237', Old clientID
-			
-			//My clientID
 			clientId: 'eb4c0c67-7376-4a50-b9fe-ed01fe020b58',
-			
-			//clientId: '5829dae7-8f84-470a-b11c-6cf32545f074',
             extraQueryParameter: 'nux=1',
             //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
         },
