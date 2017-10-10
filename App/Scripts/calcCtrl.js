7edf4aaacbd5689f346f08ccd99469ffe1bab9f3
boszkl8er <kittipong.iti17@gmail.com>
2017-10-10 14:36:15 +0700

    Update code

--- App/Scripts/calcCtrl.js
+++ App/Scripts/calcCtrl.js
@@ -2,9 +2,8 @@
 angular.module('catApp')
 .controller('calcCtrl', ['$scope', 'adalAuthenticationService', function ($scope, adalService) {
 	$scope.getToken2 = function(){
-		 var resource = 'eb4c0c67-7376-4a50-b9fe-ed01fe020b58';
-		 var resourceAdal = adalService.getClientID();
-		 console.log('Adal ClientID = '+resourceAdal);
+		 var resource = adalService.getClientID();
+		 console.log('ClientID = '+resource);
 		 var tokenVal = adalService.getCachedToken(resource);
 		 return tokenVal;
 	};
